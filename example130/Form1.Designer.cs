e87779b516f202972944b1151f2abe0a48c74aa4
Woongna <shanung@naver.com>
2022-12-10 00:56:58 +0900

    초기 커밋.

---
+++ example130/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace example130
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.White;
+            this.richTextBox2.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox2.Location = new System.Drawing.Point(314, 156);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(238, 182);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(209)))), ((int)(((byte)(107)))));
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(209)))), ((int)(((byte)(107)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(562, 173);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 152);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "한국어",
+            "영어",
+            "일본어",
+            "중국어(간체)",
+            "중국어(번체)",
+            "베트남어",
+            "인도네시아어",
+            "태국어",
+            "독일어",
+            "러시아어",
+            "스페인어",
+            "이탈리아어",
+            "프랑스어"});
+            this.comboBox1.Location = new System.Drawing.Point(367, 119);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 33);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "영어";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(253)))));
+            this.label1.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(289, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "팀명 : 한손클라우드";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "한국어",
+            "영어",
+            "일본어",
+            "중국어(간체)",
+            "중국어(번체)",
+            "베트남어",
+            "인도네시아어",
+            "태국어",
+            "독일어",
+            "러시아어",
+            "스페인어",
+            "이탈리아어",
+            "프랑스어"});
+            this.comboBox2.Location = new System.Drawing.Point(65, 118);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 33);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.Text = "한국어";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(253)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(253)))));
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(253)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(209)))), ((int)(((byte)(107)))));
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(256, 216);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(53, 69);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "▶";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(704, 100);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 156);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(240, 182);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(326, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "201895053 손 한 웅";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(325, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 21);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "201895098 한 수 민";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(576, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "번역!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(25, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 45);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "클라우드컴퓨팅";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("한컴 말랑말랑 Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(32, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 35);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Final Project";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(253)))));
+            this.ClientSize = new System.Drawing.Size(728, 349);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ example130/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace example130
+{
+    public partial class Form1 : Form
+    {
+        bool direct = false;
+        //false면 한국어->다른나라언어
+        //true면 나른나라언어에서 -> 한국어
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://openapi.naver.com/v1/papago/n2mt";
+            string client_id = "KiGVFReRf7J4nEJA1prv";
+            string client_secret = "51G1ca5rb8";
+
+
+            WebRequest wr = WebRequest.Create(url);
+            wr.Headers.Add("X-Naver-Client-Id", client_id);
+            wr.Headers.Add("X-Naver-Client-Secret", client_secret);
+            wr.Method = "POST";
+
+            string query = "";
+
+            string source = "";
+            string target = "";
+            if (direct)
+            {
+                //다른언어를 한국어로
+                query = richTextBox2.Text;
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "한국어" :
+                        source = "ko";
+                        break;
+                    case "영어":
+                        source = "en";
+                        break;
+                    case "일본어":
+                        source = "ja";
+                        break;
+                    case "중국어(간체)":
+                        source = "zh-CN";
+                        break;
+                    case "중국어(번체)":
+                        source = "zh-TW";
+                        break;
+                    case "베트남어":
+                        source = "vi";
+                        break;
+                    case "인도네시아어":
+                        source = "id";
+                        break;
+                    case "태국어":
+                        source = "th";
+                        break;
+                    case "독일어":
+                        source = "de";
+                        break;
+                    case "러시아어":
+                        source = "ru";
+                        break;
+                    case "스페인어":
+                        source = "es";
+                        break;
+                    case "이탈리아어":
+                        source = "it";
+                        break;
+                    case "프랑스어":
+                        source = "fr";
+                        break;
+                    default :
+                        break;
+                }
+                switch (comboBox2.SelectedItem.ToString())
+                {
+                    case "한국어":
+                        target = "ko";
+                        break;
+                    case "영어":
+                        target = "en";
+                        break;
+                    case "일본어":
+                        target = "ja";
+                        break;
+                    case "중국어(간체)":
+                        target = "zh-CN";
+                        break;
+                    case "중국어(번체)":
+                        target = "zh-TW";
+                        break;
+                    case "베트남어":
+                        target = "vi";
+                        break;
+                    case "인도네시아어":
+                        target = "id";
+                        break;
+                    case "태국어":
+                        target = "th";
+                        break;
+                    case "독일어":
+                        target = "de";
+                        break;
+                    case "러시아어":
+                        target = "ru";
+                        break;
+                    case "스페인어":
+                        target = "es";
+                        break;
+                    case "이탈리아어":
+                        target = "it";
+                        break;
+                    case "프랑스어":
+                        target = "fr";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                //한국어를 다른언어로
+                query = richTextBox1.Text;
+                switch (comboBox2.SelectedItem.ToString())
+                {
+                    case "한국어":
+                        source = "ko";
+                        break;
+                    case "영어":
+                        source = "en";
+                        break;
+                    case "일본어":
+                        source = "ja";
+                        break;
+                    case "중국어(간체)":
+                        source = "zh-CN";
+                        break;
+                    case "중국어(번체)":
+                        source = "zh-TW";
+                        break;
+                    case "베트남어":
+                        source = "vi";
+                        break;
+                    case "인도네시아어":
+                        source = "id";
+                        break;
+                    case "태국어":
+                        source = "th";
+                        break;
+                    case "독일어":
+                        source = "de";
+                        break;
+                    case "러시아어":
+                        source = "ru";
+                        break;
+                    case "스페인어":
+                        source = "es";
+                        break;
+                    case "이탈리아어":
+                        source = "it";
+                        break;
+                    case "프랑스어":
+                        source = "fr";
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "한국어":
+                        target = "ko";
+                        break;
+                    case "영어":
+                        target = "en";
+                        break;
+                    case "일본어":
+                        target = "ja";
+                        break;
+                    case "중국어(간체)":
+                        target = "zh-CN";
+                        break;
+                    case "중국어(번체)":
+                        target = "zh-TW";
+                        break;
+                    case "베트남어":
+                        target = "vi";
+                        break;
+                    case "인도네시아어":
+                        target = "id";
+                        break;
+                    case "태국어":
+                        target = "th";
+                        break;
+                    case "독일어":
+                        target = "de";
+                        break;
+                    case "러시아어":
+                        target = "ru";
+                        break;
+                    case "스페인어":
+                        target = "es";
+                        break;
+                    case "이탈리아어":
+                        target = "it";
+                        break;
+                    case "프랑스어":
+                        target = "fr";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+            byte[] byteDataParams = Encoding.UTF8.GetBytes("source=" + source + "&target=" + target + "&text=" + query);
+            wr.ContentType = "application/x-www-form-urlencoded";
+            wr.ContentLength = byteDataParams.Length;
+            Stream st = wr.GetRequestStream();
+            st.Write(byteDataParams, 0, byteDataParams.Length);
+            st.Close();
+
+            WebResponse wrs = wr.GetResponse();
+            Stream stream = wrs.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string text = reader.ReadToEnd();
+            stream.Close();
+            wrs.Close();
+            reader.Close();
+
+            JObject json = JObject.Parse(text);
+
+            if (direct)
+            {
+                richTextBox1.Text = json["message"]["result"]["translatedText"].ToString();
+            }
+            else
+            {
+                richTextBox2.Text = json["message"]["result"]["translatedText"].ToString();
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (direct)
+            {
+                //다른언어를 한국어로 바꾸는경우
+                button4.Text = "▶";
+                direct = false;
+
+            }
+            else
+            {
+                //한국어를 다른언어로 바꾸는경우
+                button4.Text = "◀";
+                direct = true;
+            }
+        }
+    }
+
+}
